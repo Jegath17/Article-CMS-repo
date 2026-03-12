69a529a50b519217deacdbca1ade8ab4238a86fd
JEGATH PRIYA S <150814017+Jegath17@users.noreply.github.com>
2026-03-12 20:28:56 +0530

    Enable logging in the Flask application
    Added logging configuration to the Flask app.

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,19 +1,12 @@
-"""
-The flask application package.
-"""
+import FlaskWebProject.views
 import logging
 from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
-Session(app)
-db = SQLAlchemy(app)
-login = LoginManager(app)
-login.login_view = 'login'
 
-import FlaskWebProject.views
+# Enable logging
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
